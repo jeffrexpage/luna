d824ff94d61ba1781c3c2d8f9dec292babb3008f
jeferson <jefersonparanaense@gmail.com>
2016-11-10 23:37:30 -0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+LunaPHP Framework
+=================
+
---
+++ SayHello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace luna;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Luna!';
+    }
+}
